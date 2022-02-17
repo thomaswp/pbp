3dc0a7aa63cc3c5ff7181fb4ad8cf8c9446412b1
Melody Griesen <jvgriese@ncsu.edu>
2022-02-17 03:27:05 -0500

    editor now properly aligned, not cut off on bottom

--- src/views/ComboEditor.vue
+++ src/views/ComboEditor.vue
@@ -1,15 +1,19 @@
 <template>
-  <!-- Menu bar across the top to allow user to return to homepage -->
-  <div class="topnav">
-    <router-link to="/homepage">To Homepage</router-link>
-  </div>
-  <Editor />
-  <!-- Because the code editor is a modal, it has to be top-level -->
-  <CodeEditor
-    v-if="showModal"
-    :data="editorData"
-    @close="showModal = false"
-  />
+    <!-- Menu bar across the top to allow user to return to homepage -->
+    <div class="topnav">
+        <router-link to="/homepage">To Homepage</router-link>
+        <!-- <span style="width:100%;" class="header-footer-item">
+        </span> -->
+    </div>
+    <div class="behind"> 
+        <Editor />
+        <!-- Because the code editor is a modal, it has to be top-level -->
+        <CodeEditor
+            v-if="showModal"
+            :data="editorData"
+            @close="showModal = false"
+        />
+    </div>
 </template>
 
 <script>
@@ -48,21 +52,39 @@ export default {
 .topnav {
   background-color: #333;
   overflow: hidden;
+  z-index: 5;
+  position: relative;
+  height: 40px;
 }
 
 /* Style the links inside the navigation bar */
 .topnav a {
-  float: right;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
+    /* fill the navbar top to bottom */
+    height: 100%;
+    /* rest on the right side of the navbar */
+    float: right;
+    /* align vertical within this element */
+    display: inline-flex;
+    align-items: center;
+    /* no vertical padding, but some left and right */
+    padding: 0px 16px;
+    /* text: near-white, nothing fancy */
+    color: #f2f2f2;
+    text-decoration: none;
+    font-size: 17px;
 }
 
 /* Change the color of links on hover */
 .topnav a:hover {
   background-color: #ddd;
   color: black;
+}
+
+/* Let the editor show behind the menu bar */
+.behind {
+    position: relative;
+    margin-top: -40px; /* opposite of topnav's height */
+    display: block;
+    z-index: 3;
 }
 </style>

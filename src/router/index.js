cc7f1004cfe32045e9fc5b03fa90fb70d24ae542
Melody Griesen <jvgriese@ncsu.edu>
2022-02-17 01:58:37 -0500

    Add default route to homepage
    later on, we will want to switch to the editor as the default

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,34 @@ import NotFound from '../views/NotFound.vue'
 
 
 const routes = [
-    {path: '/login', name: 'Login', component: Login},
-    {path: '/homepage', name: 'Home', component: Homepage},
-    {path: '/editor', name: 'Editor', component: Editor},
-    {path: '/:catchAll(.*)', name: 'NotFound', component: NotFound}
+    // Pages
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/homepage',
+        name: 'Home',
+        component: Homepage
+    },
+    {
+        path: '/editor',
+        name: 'Editor',
+        component: Editor
+    },
+    // Utility
+    // Default route
+    {
+        // TODO: later on, we probably want to default to the editor instead
+        path: '/',
+        redirect: '/homepage'
+    },
+    // Not found
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound', component: NotFound
+    }
 ]
 
 const router = createRouter({history: createWebHistory(process.env.BASE_URL), routes})

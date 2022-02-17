5fb7640cc159f016279557774ee98b9ee0f25ae7
vaputra <vaputra@ncsu.edu>
2022-02-16 23:22:41 -0500

    Added 404 page and router

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Homepage from '../views/homepage.vue'
 import Editor from '../components/Editor.vue'
 import Login from '../views/login.vue'
+import NotFound from '../views/NotFound.vue'
 
 
 const routes = [
     {path: '/login', name: 'Login', component: Login},
     {path: '/homepage', name: 'Home', component: Homepage},
-    {path: '/editor', name: 'Editor', component: Editor}
+    {path: '/editor', name: 'Editor', component: Editor},
+    {path: '/:catchAll(.*)', name: 'NotFound', component: NotFound}
 ]
 
 const router = createRouter({history: createWebHistory(process.env.BASE_URL), routes})
---
+++ src/views/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="NotFound">
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card height="400" color="transparent" flat>
+            <div class="display-3 mt-5">404 Page not found.</div>
+            <div class="grey--text lighten-5">
+              The page you are trying to get to never existed in this reality,
+              or has migrated to a parallel universe.
+            </div>
+            <a class="display-3 mt-5" href="/homepage">Back to Homepage</a>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<style>
+  .paragraph-text {
+    font-size: 18px;
+  }
+  .NotFound {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+</style>

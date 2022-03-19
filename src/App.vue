thomaswp/pbp

commit fed1ffcc5a4b6762a29f090316cd9b0a16ff7cae

mark most style tags as scoped

src/App.vue
@@ -12,7 +12,7 @@
 <script>
 </script>
 
-<style>
+<style> /* not scoped: global */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

src/components/CodeEditor.vue
@@ -234,7 +234,7 @@ export default {
   }
 }
 </script>
-<style>
+<style> /* why is this global, i.e. not `scoped`? */
 .readonly-highlight{
     background-color: rgb(0, 26, 110);
     opacity: 0.1;

src/components/Editor.vue
@@ -154,7 +154,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 body {
   margin: 0;
 }

src/views/ComboEditor.vue
@@ -294,7 +294,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* Add a black background color to the top navigation */
 .topnav {
   background-color: rgb(142, 162, 249, 0.95);

src/views/Login.vue
@@ -130,7 +130,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .center {
   margin-left: auto;
   margin-right: auto;

src/views/NotFound.vue
@@ -17,7 +17,7 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 .paragraph-text {
   font-size: 18px;
 }

commit ccd7cbdb59fb3feae6b8709f8a41a21477fc03bb

Fix margin to 0 to fix homepage & others
was broken by fed1ffcc5a4b6762a29f090316cd9b0a16ff7cae

src/App.vue
@@ -19,4 +19,10 @@
   color: #2c3e50;
   /* margin-top: 60px; */
 }
+
+/* Chrome defaults to 8px margin.
+Bootstrap sets this to 0, but w/o Bootstrap, we gotta do it ourselves */
+body {
+  margin: 0px
+}
 </style>
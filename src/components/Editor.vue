thomaswp/pbp

commit fed1ffcc5a4b6762a29f090316cd9b0a16ff7cae

mark most style tags as scoped

src/components/Editor.vue
@@ -154,7 +154,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 body {
   margin: 0;
 }

src/views/ComboEditor.vue
@@ -294,7 +294,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* Add a black background color to the top navigation */
 .topnav {
   background-color: rgb(142, 162, 249, 0.95);

src/App.vue
@@ -12,7 +12,7 @@
 <script>
 </script>
 
-<style>
+<style> /* not scoped: global */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

src/components/CodeEditor.vue
@@ -234,7 +234,7 @@ export default {
   }
 }
 </script>
-<style>
+<style> /* why is this global, i.e. not `scoped`? */
 .readonly-highlight{
     background-color: rgb(0, 26, 110);
     opacity: 0.1;

src/views/Login.vue
@@ -130,7 +130,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .center {
   margin-left: auto;
   margin-right: auto;

src/views/NotFound.vue
@@ -17,7 +17,7 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 .paragraph-text {
   font-size: 18px;
 }

commit 330cdb1fd09818f374bfa75c341772cf391e3bdd

bug: fix style in Editor.vue
broken by fed1ffc

src/components/Editor.vue
@@ -155,8 +155,10 @@ export default {
 </script>
 
 <style scoped>
-body {
-  margin: 0;
+
+.container {
+  flex: 1;
+  overflow: hidden;
 }
 
 .node-editor {
@@ -164,22 +166,6 @@ body {
   height: 100vh;
   width: 100vw;
   resize: vertical;
-}
-
-.node .control > input,
-.node .input-control > input {
-  width: 140px;
-}
-
-select,
-input {
-  width: 100%;
-  border-radius: 30px;
-  background-color: white;
-  padding: 2px 6px;
-  border: 1px solid #999;
-  font-size: 110%;
-  width: 170px;
 }
 
 .editor {
@@ -200,39 +186,45 @@ input {
   /* opacity: 0.7; */
 }
 
-.dock-item {
+
+/* Selectors using v-deep to style inside of child components */
+
+/* Blocks in the bottom dock */
+.dock::v-deep .dock-item {
   display: inline-block;
   vertical-align: top;
   transform: scale(0.8);
   transform-origin: 50% 0;
 }
 
-.container {
-  flex: 1;
-  overflow: hidden;
+/* style inputs inside of blocks */
+.editor::v-deep .node .control > input,
+.editor::v-deep .node .input-control > input {
+  border-radius: 30px;
+  background-color: white;
+  padding: 2px 6px;
+  border: 1px solid #999;
+  font-size: 110%;
+  width: 140px;
 }
 
-.socket.number-value {
+/* color the sockets based on data type */
+.editor::v-deep .socket.number-value {
   background: #3647df;
 }
-
-.socket.list-value {
+.editor::v-deep .socket.list-value {
   background: #c4021c;
 }
-
-.socket.loop-value {
+.editor::v-deep .socket.loop-value {
   background: #c9c616;
 }
-
-.socket.predicate-value {
+.editor::v-deep .socket.predicate-value {
   background: #30810d;
 }
-
-.socket.string-value {
+.editor::v-deep .socket.string-value {
   background: #490d81;
 }
-
-.socket.boolean-value {
+.editor::v-deep .socket.boolean-value {
   background: #42b112;
 }
 </style>

src/views/ComboEditor.vue
@@ -336,6 +336,17 @@ export default {
   z-index: 3;
 }
 
+select,
+input {
+  width: 100%;
+  border-radius: 30px;
+  background-color: white;
+  padding: 2px 6px;
+  border: 1px solid #999;
+  font-size: 110%;
+  width: 170px;
+}
+
 .curve_edge {
   border-radius: 10px;
 }